9ef0bd7db25692c141ab5cac309e5124352c5e82
so2toll <steven.otu@gmail.com>
2022-09-07 18:11:26 -0700

    feat: no lint2

--- netlify/edge-functions/get_db/get_db.js
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line no-unused-vars
-export default async (request) => {
-  // eslint-disable-next-line no-undef
-  return new Response('Hsello, World!', {
-    headers: { 'content-type': 'text/html' },
-  });
-};
