5ef2d2a5775abd9d741ea8c833597b54925d11ea
so2toll <steven.otu@gmail.com>
2022-09-08 01:30:43 -0700

    feat: neodri test

--- netlify/functions/neoserver/neoserver.ts
+++ netlify/functions/neoserver/neoserver.ts
@@ -25,10 +25,44 @@ export const handler: Handler = async (event, context) => {
   console.log("r222esponse");
   console.log("4444response");
   // console.log(data);
+
+  // npm install --save neo4j-driver
+// node example.js
+const neo4j = require('neo4j-driver');
+const driver = neo4j.driver(process.env.NEO4J_URI,
+                  neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASSWORD), 
+                  {/* encrypted: 'ENCRYPTION_OFF' */});
+
+const query =
+  `
+  MATCH (movie:Movie {title:$favorite})<-[:ACTED_IN]-(actor)-[:ACTED_IN]->(rec:Movie)
+   RETURN distinct rec.title as title LIMIT 20
+  `;
+
+const params = {"favorite": "The Matrix"};
+
+const session = driver.session({database:"neo4j"});
+
+const firstTitle = await session.run(query, params)
+  .then((result) => {
+    const allTitles = [];
+    result.records.forEach((record) => {
+        console.log(record.get('title'));
+        allTitles.push(record.get('title'));
+        
+    });
+    session.close();
+    driver.close();
+    return allTitles;
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+  console.log(firstTitle[2], "this is now here at title response");
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: `ttereHellos, ${getUrl2.category}!`,
+      message: `ttereHellos, ${firstTitle[2]}!`,
     }),
   }
 }
