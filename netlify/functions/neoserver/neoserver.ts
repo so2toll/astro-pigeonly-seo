f18f2728672dd3a598550faeb374ce30cba1f4ae
so2toll <steven.otu@gmail.com>
2022-09-08 17:26:37 -0700

    feat: static paths test

--- netlify/functions/neoserver/neoserver.ts
+++ netlify/functions/neoserver/neoserver.ts
@@ -3,27 +3,27 @@ import fetch from "node-fetch";
 
 export const handler: Handler = async (event, context) => {
   const { name = 'sYtranger' } = event.queryStringParameters;
-  const getUrl = await fetch('https://jsonplaceholder.typicode.com/todos/1').then(response => response.json())
-  .then(json => {
-    console.log(11222)
-    console.log(json);
-    return json}
-  );
-  const getUrl2 = await fetch('https://dummyjson.com/products/1') 
-  .then(response => response.json())
-  .then(json => {
-    console.log(11222)
-    console.log(json);
-    return json}
-  );
-  console.log(getUrl);
-  console.log(getUrl2);
+  // const getUrl = await fetch('https://jsonplaceholder.typicode.com/todos/1').then(response => response.json())
+  // .then(json => {
+  //   console.log(11222)
+  //   console.log(json);
+  //   return json}
+  // );
+  // const getUrl2 = await fetch('https://dummyjson.com/products/1') 
+  // .then(response => response.json())
+  // .then(json => {
+  //   console.log(11222)
+  //   console.log(json);
+  //   return json}
+  // );
+  // console.log(getUrl);
+  // console.log(getUrl2);
   // const data = await fetch('https://example.com/movies.json')
   // const data = await getUrl[0];
-  console.log("response");
-  console.log(name);
-  console.log("r222esponse");
-  console.log("4444response");
+  // console.log("response");
+  // console.log(name);
+  // console.log("r222esponse");
+  // console.log("4444response");
   // console.log(data);
 
   // npm install --save neo4j-driver
